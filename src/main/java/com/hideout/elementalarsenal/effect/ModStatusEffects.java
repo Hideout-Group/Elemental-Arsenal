89ca31f99f7505ce468983cbb03d7fe1444c16a4
Overcontrol1 <rmaloney33@gmail.com>
2023-06-25 02:05:38 +1000

    Electrified Hearts

--- src/main/java/com/hideout/elementalarsenal/effect/ModStatusEffects.java
+++ src/main/java/com/hideout/elementalarsenal/effect/ModStatusEffects.java
@@ -1,18 +1,33 @@
 package com.hideout.elementalarsenal.effect;
 
 import com.hideout.elementalarsenal.ElementalArsenal;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModStatusEffects {
+    private static final ArrayList<StatusEffect> EFFECTS = new ArrayList<>();
     public static StatusEffect ELECTRIFIED = registerEffect("electrified", new ElectrifiedStatusEffect());
     public static void registerStatusEffects() {
 
     }
 
+    public static boolean hasModEffect(LivingEntity entity) {
+        for (StatusEffectInstance effect : entity.getStatusEffects()) {
+            if (EFFECTS.contains(effect.getEffectType()))
+                return true;
+        }
+        return false;
+    }
+
     private static StatusEffect registerEffect(String name, StatusEffect effect) {
+        EFFECTS.add(effect);
         return Registry.register(Registries.STATUS_EFFECT, new Identifier(ElementalArsenal.MOD_ID, name), effect);
     }
 }
