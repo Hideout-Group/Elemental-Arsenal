1911f0fe01f5506b50678554653e32b3bbb8a4e0
Overcontrol1 <rmaloney33@gmail.com>
2023-06-25 19:50:27 +1000

    Added lightning passive

--- src/main/java/com/hideout/elementalarsenal/mixin/PlayerEntityMixin.java
+++ src/main/java/com/hideout/elementalarsenal/mixin/PlayerEntityMixin.java
@@ -1,19 +1,24 @@
 package com.hideout.elementalarsenal.mixin;
 
 import com.hideout.elementalarsenal.item.custom.interfaces.ElementalItem;
+import com.hideout.elementalarsenal.item.custom.interfaces.MultiElementItem;
 import com.hideout.elementalarsenal.util.ElementalType;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageSources;
 import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
@@ -27,6 +32,20 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         if (getStackInHand(Hand.MAIN_HAND).getItem() instanceof ElementalItem item) {
             if (item.getType(getStackInHand(Hand.MAIN_HAND)) == ElementalType.AIR) {
                 cir.setReturnValue(true);
+            }
+        }
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    public void injectTick(CallbackInfo ci) {
+        PlayerEntity player = (PlayerEntity) (Object) this;
+        ItemStack stack = player.getMainHandStack();
+
+        if (stack.getItem() instanceof ElementalItem item) {
+            switch (item.getType(stack)) {
+                case LIGHTNING -> {
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1, 2));
+                }
             }
         }
     }
